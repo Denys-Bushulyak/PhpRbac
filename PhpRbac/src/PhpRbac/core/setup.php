8773e2b2043a421b7b881adb48da1899720cd7f9
Aleksander <aleksander.lanes@gmail.com>
2014-01-01 22:53:58 +0100

    Fixing an error when using mysqli

--- PhpRbac/src/PhpRbac/core/setup.php
+++ PhpRbac/src/PhpRbac/core/setup.php
@@ -4,12 +4,12 @@
 if ($adapter=="pdo_mysql")
 {
 	try {
-		Jf::$Db=new PDO("mysql:host={$host};dbname={$dbname}",$user,$pass);
+		jf::$Db=new PDO("mysql:host={$host};dbname={$dbname}",$user,$pass);
 	}
 	catch (PDOException $e)
 	{
 		if ($e->getCode()==1049) //database not found
-			installPdoMysql($host,$user,$pass,$dbname);
+			InstallPDOMySQL($host,$user,$pass,$dbname);
 		else
 			throw $e;
 	}
@@ -17,53 +17,53 @@ if ($adapter=="pdo_mysql")
 elseif ($adapter=="pdo_sqlite")
 {
 	if (!file_exists($dbname))
-		installPdoSqlite($host,$user,$pass,$dbname);
+		InstallPDOSQLite($host,$user,$pass,$dbname);
 	else
-		Jf::$Db=new PDO("sqlite:{$dbname}",$user,$pass);
-// 		Jf::$Db=new PDO("sqlite::memory:",$user,$pass);
+		jf::$Db=new PDO("sqlite:{$dbname}",$user,$pass);
+// 		jf::$Db=new PDO("sqlite::memory:",$user,$pass);
 }
-else # default to mysqli
+else # default to mysqli 
 {
-	Jf::$Db=new mysqli($host,$user,$pass,$dbname);
-	if(Jf::$Db->connect_errno==1049);
-		installMysqli($host,$user,$pass,$dbname);
+	jf::$Db=new mysqli($host,$user,$pass,$dbname);
+	if(jf::$Db->connect_errno==1049);
+		InstallMySQLi($host,$user,$pass,$dbname);
 }
-function getSqls($dbms)
+function GetSQLs($dbms)
 {
 	$sql=file_get_contents(__DIR__."/sql/{$dbms}.sql");
-	$sql=str_replace("PREFIX_",Jf::tablePrefix(),$sql);
+	$sql=str_replace("PREFIX_",jf::tablePrefix(),$sql);
 	return explode(";",$sql);
 }
-function installPdoMysql($host,$user,$pass,$dbname)
+function InstallPDOMySQL($host,$user,$pass,$dbname)
 {
-	$sqls=getSqls("mysql");
+	$sqls=GetSQLs("mysql");
 	$db=new PDO("mysql:host={$host};",$user,$pass);
 	$db->query("CREATE DATABASE {$dbname}");
 	$db->query("USE {$dbname}");
 	if (is_array($sqls))
 		foreach ($sqls as $query)
 		$db->query($query);
-	Jf::$Db=new PDO("mysql:host={$host};dbname={$dbname}",$user,$pass);
-	Jf::$Rbac->reset(true);
+	jf::$Db=new PDO("mysql:host={$host};dbname={$dbname}",$user,$pass);
+	jf::$RBAC->Reset(true);
 }
-function installPdoSqlite($host,$user,$pass,$dbname)
+function InstallPDOSQLite($host,$user,$pass,$dbname)
 {
-	Jf::$Db=new PDO("sqlite:{$dbname}",$user,$pass);
-	$sqls=getSqls("sqlite");
+	jf::$Db=new PDO("sqlite:{$dbname}",$user,$pass);
+	$sqls=GetSQLs("sqlite");
 	if (is_array($sqls))
 		foreach ($sqls as $query)
-		Jf::$Db->query($query);
-	Jf::$Rbac->reset(true);
+		jf::$Db->query($query);
+	jf::$RBAC->Reset(true);
 }
-function installMysqli($host,$user,$pass,$dbname)
+function InstallMySQLi($host,$user,$pass,$dbname)
 {
-	$sqls=getSqls("mysql");
+	$sqls=GetSQLs("mysql");
 	$db=new mysqli($host,$user,$pass);
 	$db->query("CREATE DATABASE {$dbname}");
 	$db->select_db($dbname);
 	if (is_array($sqls))
 		foreach ($sqls as $query)
 		$db->query($query);
-	Jf::$Db=new mysqli($host,$user,$pass,$dbname);
-	Jf::$Rbac->reset(true);
+	jf::$Db=new mysqli($host,$user,$pass,$dbname);
+	jf::$RBAC->Reset(true);
 }
